1f8872254033819d8b730c7e1145f9d941107c2d
NathanLombardRetro <nlombard@retrorabbit.co.za>
2024-03-18 22:33:42 +0200

    updated login button

--- lib/ui/login/login_page.dart
+++ lib/ui/login/login_page.dart
@@ -12,155 +12,172 @@ class _LoginPageState extends State<LoginPage> {
   final TextEditingController _passwordController = TextEditingController();
 
   @override
-Widget build(BuildContext context) {
-  return Scaffold(
-    resizeToAvoidBottomInset: false,
-    backgroundColor: Color.fromARGB(255, 253, 253, 252),
-    body: SingleChildScrollView(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          // Add logo here
-          Image.asset(
-            'assets/vitaflow.png', 
-            width: 300,
-            height: 300,
-          ),
-          SizedBox(height: 16.0),
-          Padding(
-            padding: EdgeInsets.symmetric(vertical: 2.0),
-            child: Text(
-              'Login',
-              style: TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
-                color: Color(0xFF26547C),
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Color.fromARGB(255, 253, 253, 252),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            // Add logo here
+            Image.asset(
+              'assets/vitaflow.png',
+              width: 300,
+              height: 300,
+            ),
+            SizedBox(height: 16.0),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 2.0),
+              child: Text(
+                'Login',
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xFF26547C),
+                ),
               ),
             ),
-          ),
-          Padding(
-            padding: EdgeInsets.all(16.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                TextField(
-                  controller: _usernameController,
-                  decoration: InputDecoration(
-                    labelText: 'Username',
+            Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  TextField(
+                    controller: _usernameController,
+                    decoration: InputDecoration(
+                      labelText: 'Username',
+                    ),
                   ),
-                ),
-                SizedBox(height: 16.0),
-                TextField(
-                  controller: _passwordController,
-                  decoration: InputDecoration(
-                    labelText: 'Password',
+                  SizedBox(height: 16.0),
+                  TextField(
+                    controller: _passwordController,
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                    ),
+                    obscureText: true,
                   ),
-                  obscureText: true,
-                ),
-                SizedBox(height: 16.0),
-                ElevatedButton(
-                  onPressed: () {
-                    _login();
-                  },
-                  child: Text('Sign in'),
-                ),
-                SizedBox(height: 16.0),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    GestureDetector(
-                      onTap: () {
-                        
-                      },
-                      child: Text(
-                        'Forgot password?',
-                        style: TextStyle(color: Colors.black),
+                  SizedBox(height: 16.0),
+                  ElevatedButton(
+                    onPressed: () {
+                      _login();
+                    },
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(
+                          Color(0xFF26547C)),
+                      foregroundColor: MaterialStateProperty.all<Color>(
+                          Color.fromARGB(
+                              255, 253, 253, 252)),
+                      textStyle: MaterialStateProperty.all<TextStyle>(
+                          TextStyle(fontSize: 14)),
+                      padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+                          EdgeInsets.all(15)),
+                      shape: MaterialStateProperty.all<OutlinedBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(
+                              20),
+                          side: BorderSide(
+                              color:
+                                  Color(0xFF26547C)),
+                        ),
                       ),
                     ),
-                    SizedBox(width: 20),
-                    GestureDetector(
-                      onTap: () {
-                        
-                      },
-                      child: Text(
-                        'Reset password',
-                        style: TextStyle(color: Color(0xFF26547C)),
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 20),
-                Text(
-                  'Don\'t have an account? ',
-                  style: TextStyle(fontSize: 16),
-                ),
-                GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => RegisterPage()),
-                    );
-                  },
-                  child: Text(
-                    'Sign up',
-                    style: TextStyle(fontSize: 16, color: Color(0xFF26547C)),
+                    child: Text('Sign in'),
                   ),
-                ),
-              ],
+                  SizedBox(height: 16.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      GestureDetector(
+                        onTap: () {},
+                        child: Text(
+                          'Forgot password?',
+                          style: TextStyle(color: Colors.black),
+                        ),
+                      ),
+                      SizedBox(width: 20),
+                      GestureDetector(
+                        onTap: () {},
+                        child: Text(
+                          'Reset password',
+                          style: TextStyle(color: Color(0xFF26547C)),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  Text(
+                    'Don\'t have an account? ',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => RegisterPage()),
+                      );
+                    },
+                    child: Text(
+                      'Sign up',
+                      style: TextStyle(fontSize: 16, color: Color(0xFF26547C)),
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
-    ),
-  );
-}
-  void _login() async{
+    );
+  }
+
+  void _login() async {
     String username = _usernameController.text;
     String password = _passwordController.text;
 
-    showDialog(context: context, builder: (context){
-      return Center(
-        child:CircularProgressIndicator(),
-      );
-    },
+    showDialog(
+      context: context,
+      builder: (context) {
+        return Center(
+          child: CircularProgressIndicator(),
+        );
+      },
     );
-    
-    try{
-      await FirebaseAuth.instance.signInWithEmailAndPassword(email: username,password: password);
-      Navigator.pop(context);  
-    }on FirebaseAuthException catch (e){
-      
+
+    try {
+      await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: username, password: password);
       Navigator.pop(context);
-      
-      if(e.code != '')
-      {
+    } on FirebaseAuthException catch (e) {
+      Navigator.pop(context);
+
+      if (e.code != '') {
         wrongEmailMessage();
       }
     }
   }
 
-  void wrongEmailMessage(){
-    showDialog
-    (context: context,
-     builder: (context){
+  void wrongEmailMessage() {
+    showDialog(
+      context: context,
+      builder: (context) {
         return const AlertDialog(
           title: Text("Incorrect Email address"),
         );
-     },
+      },
     );
   }
 
-  void wrongPasswordmessage()
-  {
-    showDialog
-    (context: context,
-     builder: (context){
+  void wrongPasswordmessage() {
+    showDialog(
+      context: context,
+      builder: (context) {
         return const AlertDialog(
           title: Text("Incorrect password"),
         );
-     },
+      },
     );
   }
 }

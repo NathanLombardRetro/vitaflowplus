b07fbe36120bfc5ca7d6b6bbc1a6433e235a173e
NathanLombardRetro <nlombard@retrorabbit.co.za>
2024-03-25 23:46:30 +0200

    sleep model

---
+++ lib/models/sleep_model.dart
@@ -0,0 +1,12 @@
+class Sleep {
+  final DateTime date;
+  final double duration;
+  final String userId;
+  
+
+  Sleep({
+    required this.date,
+    required this.duration,
+    required this.userId,
+  });
+}

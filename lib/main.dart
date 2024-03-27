79e8fca20ad6c8b10422176cbb5eea143c1edbf9
NathanLombardRetro <nlombard@retrorabbit.co.za>
2024-03-27 09:58:46 +0200

    removed unused import

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:theme_provider/theme_provider.dart';
 import 'package:vitaflowplus/firebase_options.dart';
 import 'package:vitaflowplus/ui/login/auth_page.dart';
-//import 'package:vitaflowplus/ui/login/login_page.dart'; // Importing the LoginPage class from login_page.dart
 
 Future<void> main() async{
   WidgetsFlutterBinding.ensureInitialized();

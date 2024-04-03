79f807a73320e9e1ea454f4c279899a8c70b75e6
NathanLombardRetro <nlombard@retrorabbit.co.za>
2024-04-03 18:17:47 +0200

    new functionality

--- lib/components/top_navigation.dart
+++ lib/components/top_navigation.dart
@@ -1,20 +1,43 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:theme_provider/theme_provider.dart';
+import 'package:vitaflowplus/services/firebaseFunctions.dart';
 import 'package:vitaflowplus/ui/login/auth_page.dart';
+import 'package:vitaflowplus/ui/profile/profile_page.dart';
 
 class CustomAppBar extends StatelessWidget {
+  final user = FirebaseAuth.instance.currentUser!;
+
   @override
   Widget build(BuildContext context) {
     return AppBar(
       backgroundColor: Color(0xFF26547C),
       leading: Padding(
         padding: EdgeInsets.all(8.0),
-        child: CircleAvatar(
-          backgroundImage: AssetImage('assets/default.png'),
-          radius: 16,
-        ),
+        child: FutureBuilder<Uint8List?>(
+  future: FirebaseFunctions.getUserProfileImageData(user.uid),
+  builder: (context, snapshot) {
+    if (snapshot.connectionState == ConnectionState.waiting || snapshot.data == null) {
+      return CircleAvatar(
+        backgroundImage: AssetImage('assets/default.png'),
+        radius: 16,
+      );
+    } else if (snapshot.hasError) {
+      return CircleAvatar(
+        backgroundImage: AssetImage('assets/default.png'),
+        radius: 16,
+      );
+    } else {
+      return CircleAvatar(
+        backgroundImage: MemoryImage(snapshot.data!),
+        radius: 16,
+      );
+    }
+  },
+),
       ),
       title: FutureBuilder<DocumentSnapshot>(
         future: getUserData(),
@@ -41,7 +64,10 @@ class CustomAppBar extends StatelessWidget {
                 leading: Icon(Icons.person_2_outlined),
                 title: Text('Profile'),
                 onTap: () {
-                  Navigator.pop(context);
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => ImageUploadPage()),
+                  );
                 },
               ),
             ),
@@ -50,8 +76,7 @@ class CustomAppBar extends StatelessWidget {
                 leading: Icon(Icons.brightness_6),
                 title: Text('Switch Theme'),
                 onTap: () {
-                  ThemeProvider.controllerOf(context)
-                      .nextTheme();
+                  ThemeProvider.controllerOf(context).nextTheme();
                   Navigator.pop(context);
                 },
               ),

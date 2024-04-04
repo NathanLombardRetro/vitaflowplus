568ed1905faa8152fe757772435abc692c724e9b
NathanLombardRetro <nlombard@retrorabbit.co.za>
2024-04-04 22:58:34 +0200

    formatting

--- lib/ui/profile/profile_page.dart
+++ lib/ui/profile/profile_page.dart
@@ -74,71 +74,106 @@ class _ImageUploadPageState extends State<ImageUploadPage> {
     ThemeData currentTheme = ThemeProvider.themeOf(context).data;
     return Scaffold(
       backgroundColor: currentTheme.primaryColor,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            if (_imageFile != null)
-              Image.file(
-                _imageFile!,
-                width: 300,
-                height: 300,
-              )
-            else
-              Text('No image selected',style: TextStyle(color: currentTheme.primaryColorLight),),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _getImageTwo,
-              child: Text('Select Image'),
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all<Color>(
-                  Color.fromARGB(255, 253, 253, 252),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(height: 70,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  FloatingActionButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    backgroundColor: currentTheme.primaryColor,
+                    foregroundColor: Color(0xFF26547C),
+                    elevation: 0,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      side: BorderSide(
+                          color: currentTheme.primaryColor, width: 0),
+                    ),
+                    child: Icon(Icons.arrow_back),
+                  ),
+                  Text(
+                    'Add Workout',
+                    style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: currentTheme.primaryColorLight),
+                  ),
+                  SizedBox(width: 56),
+                ],
+              ),
+              SizedBox(height: 20),
+              if (_imageFile != null)
+                Image.file(
+                  _imageFile!,
+                  width: 300,
+                  height: 300,
+                )
+              else
+                Text(
+                  'No image selected',
+                  style: TextStyle(color: currentTheme.primaryColorLight),
                 ),
-                foregroundColor: MaterialStateProperty.all<Color>(
-                  Color(0xFF26547C),
-                ),
-                elevation: MaterialStateProperty.all<double>(0),
-                shape: MaterialStateProperty.all<OutlinedBorder>(
-                  RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    side: BorderSide(color: Color(0xFF26547C), width: 1),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: _getImageTwo,
+                child: Text('Select Image'),
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(
+                    Color.fromARGB(255, 253, 253, 252),
+                  ),
+                  foregroundColor: MaterialStateProperty.all<Color>(
+                    Color(0xFF26547C),
+                  ),
+                  elevation: MaterialStateProperty.all<double>(0),
+                  shape: MaterialStateProperty.all<OutlinedBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      side: BorderSide(color: Color(0xFF26547C), width: 1),
+                    ),
                   ),
                 ),
               ),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                if (_imageFile != null) {
-                  _addUserPicture(_imageFile!);
-                }
-              },
-              style: ButtonStyle(
-                backgroundColor:
-                    MaterialStateProperty.all<Color>(Color(0xFF26547C)),
-                foregroundColor: MaterialStateProperty.all<Color>(
-                    Color.fromARGB(255, 253, 253, 252)),
-                textStyle: MaterialStateProperty.all<TextStyle>(
-                    TextStyle(fontSize: 14)),
-                padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
-                    EdgeInsets.all(15)),
-                shape: MaterialStateProperty.all<OutlinedBorder>(
-                  RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20),
-                    side: BorderSide(color: Color(0xFF26547C)),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  if (_imageFile != null) {
+                    _addUserPicture(_imageFile!);
+                  }
+                },
+                style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Color(0xFF26547C)),
+                  foregroundColor: MaterialStateProperty.all<Color>(
+                      Color.fromARGB(255, 253, 253, 252)),
+                  textStyle: MaterialStateProperty.all<TextStyle>(
+                      TextStyle(fontSize: 14)),
+                  padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+                      EdgeInsets.all(15)),
+                  shape: MaterialStateProperty.all<OutlinedBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      side: BorderSide(color: Color(0xFF26547C)),
+                    ),
+                  ),
+                ),
+                child: Text(
+                  'Upload Image',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
                   ),
                 ),
               ),
-              child: Text(
-                'Upload Image',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );

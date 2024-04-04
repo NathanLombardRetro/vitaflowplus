fd36fefcd4fc155ce576a0d0109a61e99870fba3
NathanLombardRetro <nlombard@retrorabbit.co.za>
2024-04-04 20:05:13 +0200

    formatting and styling

--- lib/ui/register/register_page.dart
+++ lib/ui/register/register_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:theme_provider/theme_provider.dart';
 
 class RegisterPage extends StatefulWidget {
   @override
@@ -42,90 +43,194 @@ class _RegisterPageState extends State<RegisterPage> {
 
   @override
   Widget build(BuildContext context) {
+    ThemeData currentTheme = ThemeProvider.themeOf(context).data;
     return Scaffold(
-      appBar: AppBar(
-        title: Text(''),
-      ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextFormField(
-                controller: _firstNameController,
-                decoration: InputDecoration(
-                  labelText: 'First Name',
-                  border: OutlineInputBorder(),
+      backgroundColor: currentTheme.primaryColor,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          SizedBox(height: 60),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                FloatingActionButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  backgroundColor: currentTheme.primaryColor,
+                  foregroundColor: Color(0xFF26547C),
+                  elevation: 0,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    side:
+                        BorderSide(color: currentTheme.primaryColor, width: 0),
+                  ),
+                  child: Icon(Icons.arrow_back),
                 ),
-              ),
-              SizedBox(height: 16),
-              TextFormField(
-                controller: _lastNameController,
-                decoration: InputDecoration(
-                  labelText: 'Last Name',
-                  border: OutlineInputBorder(),
+                Text(
+                  "Register",
+                  style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: currentTheme.primaryColorLight),
                 ),
-              ),
-              SizedBox(height: 16),
-              TextFormField(
-                controller: _emailController,
-                decoration: InputDecoration(
-                  labelText: 'Email',
-                  border: OutlineInputBorder(),
-                ),
-              ),
-              SizedBox(height: 16),
-              TextFormField(
-                controller: _phoneNumController,
-                decoration: InputDecoration(
-                  labelText: 'Phone Number',
-                  border: OutlineInputBorder(),
-                ),
-              ),
-              SizedBox(height: 16),
-              TextFormField(
-                controller: _passwordController,
-                obscureText: true,
-                decoration: InputDecoration(
-                  labelText: 'Password',
-                  border: OutlineInputBorder(),
-                ),
-              ),
-              SizedBox(height: 32),
-              SizedBox(
-                
-                child: ElevatedButton(
-                  onPressed: _register,
-                  style: ButtonStyle(
-                    backgroundColor:
-                        MaterialStateProperty.all<Color>(Color(0xFF26547C)),
-                    foregroundColor: MaterialStateProperty.all<Color>(
-                        Color.fromARGB(255, 253, 253, 252)),
-                    textStyle: MaterialStateProperty.all<TextStyle>(
-                        TextStyle(fontSize: 14)),
-                    padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
-                        EdgeInsets.all(15)),
-                    shape: MaterialStateProperty.all<OutlinedBorder>(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20),
-                        side: BorderSide(color: Color(0xFF26547C)),
+                SizedBox(
+                    width:
+                        56),
+              ],
+            ),
+          ),
+          SizedBox(height: 20),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.0),
+            child: Text(
+              "Fill out the form below to register:",
+              style: TextStyle(
+                  fontSize: 18, color: currentTheme.primaryColorLight),
+            ),
+          ),
+          SizedBox(height: 20),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    TextFormField(
+                      controller: _firstNameController,
+                      style: TextStyle(color: currentTheme.primaryColorLight),
+                      decoration: InputDecoration(
+                        labelText: 'First Name',
+                        labelStyle:
+                            TextStyle(color: currentTheme.primaryColorLight),
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(color: Color(0xFF26547C)),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Color(0xFF26547C)),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Color(0xFF26547C)),
+                        ),
                       ),
                     ),
-                  ),
-                  child: Text(
-                    'Register',
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
+                    SizedBox(height: 16),
+                    TextFormField(
+                      controller: _lastNameController,
+                      style: TextStyle(color: currentTheme.primaryColorLight),
+                      decoration: InputDecoration(
+                        labelText: 'Last Name',
+                        labelStyle:
+                            TextStyle(color: currentTheme.primaryColorLight),
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.blue),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Color(0xFF26547C)),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Color(0xFF26547C)),
+                        ),
+                      ),
                     ),
-                  ),
+                    SizedBox(height: 16),
+                    TextFormField(
+                      controller: _emailController,
+                      style: TextStyle(color: currentTheme.primaryColorLight),
+                      decoration: InputDecoration(
+                        labelText: 'Email',
+                        labelStyle:
+                            TextStyle(color: currentTheme.primaryColorLight),
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(color: Color(0xFF26547C)),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Color(0xFF26547C)),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Color(0xFF26547C)),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 16),
+                    TextFormField(
+                      controller: _phoneNumController,
+                      style: TextStyle(color: currentTheme.primaryColorLight),
+                      decoration: InputDecoration(
+                        labelText: 'Phone Number',
+                        labelStyle:
+                            TextStyle(color: currentTheme.primaryColorLight),
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(color: Color(0xFF26547C)),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Color(0xFF26547C)),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Color(0xFF26547C)),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 16),
+                    TextFormField(
+                      controller: _passwordController,
+                      style: TextStyle(color: currentTheme.primaryColorLight),
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                        labelStyle:
+                            TextStyle(color: currentTheme.primaryColorLight),
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(color: Color(0xFF26547C)),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Color(0xFF26547C)),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Color(0xFF26547C)),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 32),
+                    SizedBox(
+                      child: ElevatedButton(
+                        onPressed: _register,
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all<Color>(
+                              Color(0xFF26547C)),
+                          foregroundColor: MaterialStateProperty.all<Color>(
+                              Color.fromARGB(255, 253, 253, 252)),
+                          textStyle: MaterialStateProperty.all<TextStyle>(
+                              TextStyle(fontSize: 14)),
+                          padding:
+                              MaterialStateProperty.all<EdgeInsetsGeometry>(
+                                  EdgeInsets.all(15)),
+                          shape: MaterialStateProperty.all<OutlinedBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20),
+                              side: BorderSide(color: Color(0xFF26547C)),
+                            ),
+                          ),
+                        ),
+                        child: Text(
+                          'Register',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
--- lib/ui/workouts/addWorkout/addWorkout.dart
+++ lib/ui/workouts/addWorkout/addWorkout.dart
@@ -1,178 +1,242 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:theme_provider/theme_provider.dart';
 import 'package:uuid/uuid.dart';
+import 'package:vitaflowplus/ui/workouts/workout/workouts.dart';
 
-class AddWorkoutPage extends StatefulWidget{
+class AddWorkoutPage extends StatefulWidget {
   AddWorkoutPage({super.key});
 
   @override
-  State <AddWorkoutPage> createState() => _MyAddWorkoutState();
+  State<AddWorkoutPage> createState() => _MyAddWorkoutState();
 }
 
-class _MyAddWorkoutState extends State<AddWorkoutPage>
-{
+class _MyAddWorkoutState extends State<AddWorkoutPage> {
   final TextEditingController _workoutNameController = TextEditingController();
   final TextEditingController _timeTrainedController = TextEditingController();
-  final TextEditingController _workoutDescriptionController = TextEditingController();
+  final TextEditingController _workoutDescriptionController =
+      TextEditingController();
   final TextEditingController _exercisesController = TextEditingController();
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   final user = FirebaseAuth.instance.currentUser!;
 
   Future<DocumentSnapshot> getUserData() async {
     final userUID = user.uid;
-    return await FirebaseFirestore.instance.collection('users').doc(userUID).get();
-    }
-
-    Future<void> _addWorkout() async {
-  try {
-    // Get user ID
-    String userId = FirebaseAuth.instance.currentUser!.uid;
-
-    String workoutName = _workoutNameController.text;
-    String timeTrained = _timeTrainedController.text;
-    String workoutDescription = _workoutDescriptionController.text;
-    List<String> exercises = _exercisesController.text.split(',');
-
-    String workoutId = Uuid().v4();
-
-    await FirebaseFirestore.instance.collection('workouts').doc(workoutId).set({
-      'userId': userId,
-      'workoutName': workoutName,
-      'timeTrained': timeTrained,
-      'workoutDescription': workoutDescription,
-      'exercises': exercises,
-      'date': DateTime.now(),
-    });
-
-    print('Workout added successfully');
-    Navigator.pop(context);
-  } catch (e) {
-    print('Failed to add workout: $e');
+    return await FirebaseFirestore.instance
+        .collection('users')
+        .doc(userUID)
+        .get();
   }
-}
 
+  Future<void> _addWorkout() async {
+    try {
+      // Get user ID
+      String userId = FirebaseAuth.instance.currentUser!.uid;
 
-    @override
+      String workoutName = _workoutNameController.text;
+      String timeTrained = _timeTrainedController.text;
+      String workoutDescription = _workoutDescriptionController.text;
+      List<String> exercises = _exercisesController.text.split(',');
+
+      String workoutId = Uuid().v4();
+
+      await FirebaseFirestore.instance
+          .collection('workouts')
+          .doc(workoutId)
+          .set({
+        'userId': userId,
+        'workoutName': workoutName,
+        'timeTrained': timeTrained,
+        'workoutDescription': workoutDescription,
+        'exercises': exercises,
+        'date': DateTime.now(),
+      });
+
+      print('Workout added successfully');
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => WorkoutsPage()),
+      );
+    } catch (e) {
+      print('Failed to add workout: $e');
+    }
+  }
+
+  @override
 Widget build(BuildContext context) {
+  ThemeData currentTheme = ThemeProvider.themeOf(context).data;
   return Scaffold(
-    appBar: AppBar(
-      leading: Padding(
-        padding: EdgeInsets.all(8.0),
-        child: CircleAvatar(
-          backgroundImage: AssetImage('assets/default.png'),
-          radius: 16,
-        ),
-      ),
-      title: FutureBuilder<DocumentSnapshot>(
-        future: getUserData(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Text('Loading...');
-          } else if (snapshot.hasError) {
-            return Text('Error: ${snapshot.error}');
-          } else {
-            final userData = snapshot.data!.data() as Map<String, dynamic>;
-            final firstName = userData['firstName'];
-            final lastName = userData['lastName'];
-            return Text('$firstName $lastName');
-          }
-        },
-      ),
-      actions: [
-        PopupMenuButton(
-          icon: Icon(Icons.settings),
-          itemBuilder: (context) => [
-            PopupMenuItem(
-              child: ListTile(
-                leading: Icon(Icons.settings),
-                title: Text('Settings'),
-                onTap: () {
-                  Navigator.pop(context);
-                },
+    backgroundColor: currentTheme.primaryColor,
+    body: Center(
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  FloatingActionButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    backgroundColor: currentTheme.primaryColor,
+                    foregroundColor: Color(0xFF26547C),
+                    elevation: 0,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      side: BorderSide(color: currentTheme.primaryColor, width: 0),
+                    ),
+                    child: Icon(Icons.arrow_back),
+                  ),
+                  Text(
+                    'Add Workout',
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: currentTheme.primaryColorLight),
+                  ),
+                  SizedBox(width: 56),
+                ],
               ),
-            ),
-            PopupMenuItem(
-              child: ListTile(
-                leading: Icon(Icons.logout),
-                title: Text('Logout'),
-                onTap: () {
-                  Navigator.pop(context);
-                },
+              SizedBox(height: 20),
+              Form(
+                key: _formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    TextFormField(
+                        controller: _workoutNameController,
+                        style: TextStyle(color: currentTheme.primaryColorLight),
+                        decoration: InputDecoration(
+                          labelText: 'Workout Name',
+                          labelStyle:
+                              TextStyle(color: currentTheme.primaryColorLight),
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide(color: Color(0xFF26547C)),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Color(0xFF26547C)),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Color(0xFF26547C)),
+                          ),
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter a workout name';
+                          }
+                          return null;
+                        },
+                      ),
+                      SizedBox(height: 16),
+                      TextFormField(
+                        controller: _timeTrainedController,
+                        style: TextStyle(color: currentTheme.primaryColorLight),
+                        decoration: InputDecoration(
+                          labelText: 'Time Trained',
+                          labelStyle:
+                              TextStyle(color: currentTheme.primaryColorLight),
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide(color: Color(0xFF26547C)),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Color(0xFF26547C)),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Color(0xFF26547C)),
+                          ),
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter the time trained';
+                          }
+                          return null;
+                        },
+                      ),
+                      SizedBox(height: 16),
+                      TextFormField(
+                        controller: _workoutDescriptionController,
+                        style: TextStyle(color: currentTheme.primaryColorLight),
+                        decoration: InputDecoration(
+                          labelText: 'Workout Description',
+                          labelStyle:
+                              TextStyle(color: currentTheme.primaryColorLight),
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide(color: Color(0xFF26547C)),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Color(0xFF26547C)),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Color(0xFF26547C)),
+                          ),
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter a workout description';
+                          }
+                          return null;
+                        },
+                      ),
+                      SizedBox(height: 16),
+                      TextFormField(
+                        controller: _exercisesController,
+                        style: TextStyle(color: currentTheme.primaryColorLight),
+                        decoration: InputDecoration(
+                          labelText: 'Exercises (comma-separated)',
+                          labelStyle:
+                              TextStyle(color: currentTheme.primaryColorLight),
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide(color: Color(0xFF26547C)),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Color(0xFF26547C)),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Color(0xFF26547C)),
+                          ),
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter at least one exercise';
+                          }
+                          return null;
+                        },
+                      ),
+                  ],
+                ),
               ),
-            ),
-          ],
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    _addWorkout();
+                  }
+                },
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(Color(0xFF26547C)),
+                  foregroundColor: MaterialStateProperty.all<Color>(Color.fromARGB(255, 253, 253, 252)),
+                  textStyle: MaterialStateProperty.all<TextStyle>(TextStyle(fontSize: 14)),
+                  padding: MaterialStateProperty.all<EdgeInsetsGeometry>(EdgeInsets.all(15)),
+                  shape: MaterialStateProperty.all<OutlinedBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      side: BorderSide(color: Color(0xFF26547C)),
+                    ),
+                  ),
+                ),
+                child: Text(
+                  'Submit',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
-      ],
-    ),
-    body: Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            'Add Workout',
-            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-          ),
-          SizedBox(height: 20),
-          Form(
-            key: _formKey,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                TextFormField(
-                  controller: _workoutNameController,
-                  decoration: InputDecoration(labelText: 'Workout Name'),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter a workout name';
-                    }
-                    return null;
-                  },
-                ),
-                TextFormField(
-                  controller: _timeTrainedController,
-                  decoration: InputDecoration(labelText: 'Time Trained'),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter the time trained';
-                    }
-                    return null;
-                  },
-                ),
-                TextFormField(
-                  controller: _workoutDescriptionController,
-                  decoration: InputDecoration(labelText: 'Workout Description'),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter a workout description';
-                    }
-                    return null;
-                  },
-                ),
-                TextFormField(
-                  controller: _exercisesController,
-                  decoration: InputDecoration(labelText: 'Exercises (comma-separated)'),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter at least one exercise';
-                    }
-                    return null;
-                  },
-                ),
-                SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: () {
-                    if (_formKey.currentState!.validate()) {
-                      _addWorkout();
-                    }
-                  },
-                  child: Text('Submit'),
-                ),
-              ],
-            ),
-          ),
-        ],
       ),
     ),
   );

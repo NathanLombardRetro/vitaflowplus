a873ca1fdc740ce27c1075926cb442db72f50239
NathanLombardRetro <nlombard@retrorabbit.co.za>
2024-05-06 21:22:23 +0200

    fixed pdf download

--- lib/services/pdfService.dart
+++ lib/services/pdfService.dart
@@ -1,12 +1,14 @@
 import 'dart:io';
+import 'dart:typed_data';
 import 'dart:ui';
 import 'package:syncfusion_flutter_pdf/pdf.dart';
 import 'package:vitaflowplus/models/sugar_model.dart';
 import 'package:vitaflowplus/services/firebaseFunctions.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:document_file_save_plus/document_file_save_plus.dart';
 
 class ReportService {
-   static Future<void> generateReport(String userId) async {
+  static Future<void> generateReport(String userId) async {
     final PdfDocument document = PdfDocument();
     final PdfPage page = document.pages.add();
     final PdfGrid grid = PdfGrid();
@@ -18,10 +20,8 @@ class ReportService {
     grid.headers[0].cells[3].value = 'Mood';
 
     try {
-      // Fetch sugar data asynchronously
       List<Sugar> sugarData = await FirebaseFunctions.fetchSugarLevels(userId);
 
-      // Add fetched data to the grid
       for (int i = 0; i < sugarData.length; i++) {
         grid.rows.add();
         grid.rows[i].cells[0].value = sugarData[i].date.toString();
@@ -30,30 +30,33 @@ class ReportService {
         grid.rows[i].cells[3].value = sugarData[i].mood;
       }
     } catch (error) {
-      // Handle error if fetching data fails
       print('Error fetching sugar data: $error');
     }
 
-    // Draw the grid on the PDF page
     grid.draw(
       page: page,
       bounds: Rect.fromLTWH(0, 0, page.getClientSize().width, page.getClientSize().height),
     );
 
-    // Get the directory for saving the PDF file
     Directory? directory = await getExternalStorageDirectory();
     if (directory != null) {
       String path = directory.path;
-
-      // Save the document to the specified path
       final File file = File('$path/report.pdf');
       final List<int> bytes = await document.save();
       await file.writeAsBytes(bytes, flush: true);
+
+      print('PDF file saved to: $path');
+
+      // Save the file using document_file_save_plus package
+      await DocumentFileSavePlus().saveMultipleFiles(
+        dataList: [Uint8List.fromList(bytes)],
+        fileNameList: ['report.pdf'],
+        mimeTypeList: ['application/pdf'],
+      );
     } else {
       print('External storage directory is null.');
     }
 
-    // Dispose the document to release resources
     document.dispose();
   }
 }
